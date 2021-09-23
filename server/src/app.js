e1263b2e83ff83741f1a446996de0e6dec88a007
taew22 <87745123+taew22@users.noreply.github.com>
2021-09-23 17:12:36 +0700

    Add files via upload

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,19 @@
 let express = require('express')
-let bodyParser = require('body-parser')
+
 const {sequelize} = require('./models')
+let cors = require('cors')
+const config = require('./config/config')
 
 const app = express()
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors())
+app.use('/assets',express.static('public'))
 
+require('./userPassport')
 require('./routes')(app)
+
 
 app.get('/status', function (req, res ){
   res.send('Hello nodejs server')
@@ -18,11 +24,10 @@ app.get('/hello/:person', function (req,res) {
   res.send('sey hello with ' + req.params.person)
 })
 
-
-let port = 8081
+let port = process.env.PORT || config.port
 
 sequelize.sync({force: false}).then(() => {
-  app.listen(port, function () {
+  app.listen(port, function() {
       console.log('Server running on ' + port)
   })
 })
--- server/src/routes.js
+++ server/src/routes.js
@@ -1,29 +1,70 @@
 const UserController = require('./controllers/UserController')
+const UserAuthenController = require('./controllers/UserAuthenController')
+const isAuthenController = require('./authen/isAuthenController')
+const BlogController = require('./controllers/BlogController')
+const CommentController = require('./controllers/CommentController')
+let multer = require("multer")
+
+//upload section
+let storage = multer.diskStorage({
+  destination: function(req, file, callback){
+    callback(null, "./public/uploads");
+  }, 
+  filename: function(req, file, callback){
+    //callback(null, filedname + '_' + Data.now());
+    console.log(file);
+    callback(null, file.originalname);
+  }
+})
+let upload = multer({ storage: storage }).array("userPhoto", 10)
 
 module.exports = (app) => {
-  /* RESFUL Api for users management */
-  // create user
-  app.post('/user',
-    UserController.create
-  )
+  app.post('/user', UserController.create)
+  app.put('/user/:userId', UserController.put)
+  app.delete('/user/:userId', UserController.remove)
+  app.get('/user/:userId', UserController.show)
+  app.get('/users', isAuthenController, UserController.index)
+  //login
+  app.post('/login', UserAuthenController.login)
+  //blog
+  app.post('/blog', BlogController.create)
+  app.put('/blog/:blogId', BlogController.put)
+  app.delete('/blog/:blogId', BlogController.remove)
+  app.get('/blog/:blogId', BlogController.show)
+  app.get('/blogs', BlogController.index)
+  //comment
+  app.post('/comment', CommentController.create)
+  app.put('/comment/:commentId', CommentController.put)
+  app.delete('/comment/:commentId', CommentController.remove)
+  app.get('/comment/:commentId', CommentController.show)
+  app.get('/comments', CommentController.index)
 
-  // edit user, suspend, active
-  app.put('/user/:userId',
-    UserController.put
-  )
+  //upload
+  app.post("/upload", function(req, res) {
+    //isUserAuthenticated,
+    upload(req, res, function(err) {
+      if(err) {
+        return res.end("Error uploading file.");
+      }
+      res.end("File is uploaded");
+    })
+  })
 
-  // delete user
-  app.delete('/user/:userId',
-    UserController.remove
-  )
-
-  // get user by id
-  app.get('/user/:userId',
-    UserController.show
-  )
-
-  // get all user
-  app.get('/users',
-    UserController.index    
-  )
+  //delete file uploaded function
+  app.post('/upload/delete', async function (req, res) {
+    try {
+      const fs = require('fs');
+      console.log(req.body.filename)
+      fs.unlink(process.cwd() + '/public/uploads'+ req.body.filename,
+      (err) => {
+        if(err) throw err;
+        res.send("Delete sucessful")
+        //console.log('successfully deleted material file');
+  });
+      } catch (err) {
+        res.status(500).send({
+          error: 'An error has occured trying to delete file the material'
+        })
+      }
+    }) 
 }
